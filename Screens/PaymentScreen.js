af522b7d52724afca2cfe483a69be06d618062ed
lkingl123 <lokekingloong@gmail.com>
2024-06-07 12:40:43 -0600

    payment implemented better removing other types

--- Screens/PaymentScreen.js
+++ Screens/PaymentScreen.js
@@ -1,17 +1,19 @@
 // PaymentScreen.js
-import React from 'react';
-import { View, StyleSheet, Alert } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, ScrollView, StyleSheet, Alert } from 'react-native';
 import { PayPalButtons, PayPalScriptProvider } from '@paypal/react-paypal-js';
 
 const CLIENT_ID = 'AeTWExkh_wQIt1Sm91ml0NTAJ2YR77araovottV1fI-2lNYVOsSkYqeG6wTI_aGsljMs2-Iv_coOiMuQ'; // Replace with your actual Client ID
 
 export default function PaymentScreen() {
+  const [amountOwed, setAmountOwed] = useState('10.00'); // Example amount owed
+
   const createOrder = (data, actions) => {
     return actions.order.create({
       purchase_units: [{
         amount: {
           currency_code: 'USD',
-          value: '10.00' // Example amount
+          value: amountOwed
         }
       }]
     });
@@ -20,28 +22,44 @@ export default function PaymentScreen() {
   const onApprove = (data, actions) => {
     return actions.order.capture().then(details => {
       Alert.alert('Payment Successful', `Transaction completed by ${details.payer.name.given_name}`);
+      // Update amount owed or perform other actions as needed
     }).catch(error => {
       Alert.alert('Payment Error', error.message);
     });
   };
 
   return (
-    <PayPalScriptProvider options={{ "client-id": CLIENT_ID, locale: 'en_US', "disable-funding": "paylater" }}>
-      <View style={styles.container}>
-        <PayPalButtons
-          createOrder={createOrder}
-          onApprove={onApprove}
-          style={{ layout: 'vertical' }}
-        />
-      </View>
+    <PayPalScriptProvider options={{ "client-id": CLIENT_ID, locale: 'en_US', "disable-funding": "paylater,venmo" }}>
+      <ScrollView contentContainerStyle={styles.container}>
+        <Text style={styles.amountOwed}>Amount Owed: ${amountOwed}</Text>
+        <View style={styles.paypalContainer}>
+          <PayPalButtons
+            fundingSource="card"
+            createOrder={createOrder}
+            onApprove={onApprove}
+            style={{ layout: 'vertical' }}
+          />
+        </View>
+      </ScrollView>
     </PayPalScriptProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    alignSelf: 'center',
     justifyContent: 'center',
     padding: 16,
-  }
+    alignItems: 'center',
+  },
+  amountOwed: {
+    fontSize: 24,
+    textAlign: 'center',
+  },
+  paypalContainer: {
+    width: '100%',
+    maxWidth: 600,
+    alignItems: 'center',
+    marginTop: 20,
+  },
 });
+
